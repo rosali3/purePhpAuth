155b158a5b646cdefbda8645d1758f205edc5f66
Розалия Набиуллина <87576995+rosali3@users.noreply.github.com>
2023-06-11 22:30:11 +0300

    html + css

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>АВТОРИЗАЦИЯ ЧИСТЫЙ PHP</title>
+    <link rel="stylesheet" href="styles/main.css">
+</head>
+<body>
+
+    <!-- Форма регистрации -->
+    <form action="" method="">
+        <label>ЛОГИН</label>
+        <input type="text" placeholder="Введите логин">
+        <label>ПАРОЛЬ</label>
+        <input type="password" placeholder="Введите пароль">
+        <button>ВОЙТИ</button>
+        <p>
+            Нет аккаунта?  <a href="/register.php"> РЕГИСТРАЦИЯ</a>
+        </p>
+    </form>
+
+</body>
+</html>
+
+<?php
---
+++ register.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title> РЕГИСТРАЦИЯ ЧИСТЫЙ PHP </title>
+    <link rel="stylesheet" href="styles/main.css">
+</head>
+<body>
+
+<!-- Форма регистрации -->
+<form action="" method="">
+    <label>Имя</label>
+    <input type="text" placeholder="Введите имя">
+    <label>Почта</label>
+    <input type="email" placeholder="Введите email">
+    <label>Аватарка профиля</label>
+    <input type="file">
+    <label>Придумайте пароль</label>
+    <input type="password" placeholder="Введите пароль">
+    <label>Повторите пароль</label>
+    <input type="password" placeholder="Повторите введенный пароль">
+    <button>Регистрация</button>
+    <p>
+        Есть аккаунт?  <a href="/"> Войти</a>
+    </p>
+</form>
+
+</body>
+</html>
+
+<?php
